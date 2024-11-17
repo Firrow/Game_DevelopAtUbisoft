d745b89829f0f98cb7617e55d6310ae115a1ea6d
Firrow <firrowmb@gmail.com>
2024-11-17 12:54:56 +0100

    init project
    Adding plugin for Visual Studio

---
+++ Project_Mira_DaU/Source/Project_Mira_DaU/Project_Mira_DaU.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Project_Mira_DaU.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project_Mira_DaU, "Project_Mira_DaU" );
