305f4a6a3c2f4a883142937222506df574603171
Firrow <firrowmb@gmail.com>
2024-12-04 20:57:43 +0100

    Add effet to Watch_ressource + Update timer system

--- Project_Mira_DaU/Source/Project_Mira_DaU/Public/Game/GameManager.h
+++ Project_Mira_DaU/Source/Project_Mira_DaU/Public/Game/GameManager.h
@@ -12,11 +12,9 @@ class PROJECT_MIRA_DAU_API AGameManager : public AActor
 	
 private:	
 	int SECONDS_IN_A_DAY = 86400;
-	int TOTAL_PLAYING_TIME = 10; //TODO : Set to 3600 when tests are finished
+	int TOTAL_PLAYING_TIME = 10; //TODO : Set to 3600 or 1800 when tests are finished
 
-	FTimerHandle timer1;
-	FTimerHandle timer2;
-	int RealTimeValue;
+	FTimerHandle timer;
 	int translateSecondsIRLTimeToGameTime = SECONDS_IN_A_DAY / TOTAL_PLAYING_TIME;
 
 	AGameManager();
@@ -29,6 +27,7 @@ protected:
 
 
 public:	
+	int RealTimeValue;
 	int GameTimeValue;
 
 	virtual void Tick(float DeltaTime) override;
