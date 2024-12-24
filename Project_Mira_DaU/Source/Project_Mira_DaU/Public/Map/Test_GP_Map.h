76bd71e9dd43b8dd74c8072065992dba799b0ac6
Firrow <firrowmb@gmail.com>
2024-12-24 18:54:22 +0100

    Generate the floor + the first line of buildings

--- Project_Mira_DaU/Source/Project_Mira_DaU/Public/Map/Test_GP_Map.h
+++ Project_Mira_DaU/Source/Project_Mira_DaU/Public/Map/Test_GP_Map.h
@@ -9,6 +9,19 @@
 class PaperTileMapComponent;
 class UPaperTileSet;
 class UPaperTileMap;
+class UPaperTileLayer;
+
+
+UENUM(BlueprintType)
+enum class ETiles : uint8 {
+	None = 0 UMETA(DisplayName = "None"),  // Valeur par défaut
+	GROUND = 0 UMETA(DisplayName = "GROUND"),
+	STARTBUILDING = 25 UMETA(DisplayName = "STARTBUILDING"),
+	BUILDINGWALL = 26 UMETA(DisplayName = "BUILDINGWALL"),
+	ENDBUILDING = 27 UMETA(DisplayName = "ENDBUILDING"),
+	TEMP = 48 UMETA(DisplayName = "TEMP")
+};
+
 
 UCLASS()
 class PROJECT_MIRA_DAU_API ATest_GP_Map : public AActor
@@ -20,9 +33,14 @@ private:
 	int GridWidth;
 	int GridHeight;
 	float TileSize;
+	int BuildingWidth = 0;
 
 	ATest_GP_Map();
 	void GenerateWorld();
+
+	bool LeftNeighborIsNull(UPaperTileLayer& layer, int x, int y);
+	bool CurrentTileIsOnGround(UPaperTileLayer& layer, int x, int y);
+	bool BuildBuildingOrNot(int const probability);
 
 protected:
 	virtual void BeginPlay() override;
@@ -35,4 +53,24 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Map Settings")
 	UPaperTileMapComponent* MyTileMapComponent;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Generation Map Settings")
+	int PROBA_START_BUILDING = 65;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Generation Map Settings")
+	int PROBA_EXTEND_BUILD_WIDTH = PROBA_START_BUILDING;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Generation Map Settings")
+	int PROBA_EXTEND_BUILD_HEIGHT = 50;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Generation Map Settings")
+	int MIN_WIDTH_BUILDING = 7;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Generation Map Settings")
+	int MAX_WIDTH_BUILDING = 20;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Generation Map Settings")
+	int MIN_HEIGHT_BUILDING = 7;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Generation Map Settings")
+	int MAX_HEIGHT_BUILDING = 14;
 };
