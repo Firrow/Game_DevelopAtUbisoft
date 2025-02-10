7f3293ab636f4785d9b610837e43764a4cea2c5d
Firrow <firrowmb@gmail.com>
2025-02-10 23:36:11 +0100

    Fix bug door on ladder

--- Project_Mira_DaU/Source/Project_Mira_DaU/Public/Map/Test_GP_Map.h
+++ Project_Mira_DaU/Source/Project_Mira_DaU/Public/Map/Test_GP_Map.h
@@ -65,7 +65,7 @@ private:
 	int CountTiles(UPaperTileLayer& layer, int x, int y, TFunction<bool(UPaperTileLayer&, int, int)> condition, TFunction<void(int&, int&)> iteration);
 	void PutTileOnGrid(int const x, int const y, int32 tile, UPaperTileLayer& layer);
 	FVector ConvertGridPositionToWorldPosition(const int x, const int y);
-	void SpawnBPTile(TSubclassOf<AInteractible>& BPTile, const int x, const int y);
+	void SpawnBPTile(TSubclassOf<AInteractible>& BPTile, const int x, const int y, int BPSize);
 	AInteractible* FindInteractibleAtGridPosition(int x, int y);
 
 	bool BuildOrNot(int const probability);
