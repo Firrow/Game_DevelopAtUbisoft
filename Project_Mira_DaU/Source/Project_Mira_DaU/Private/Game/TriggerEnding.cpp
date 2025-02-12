afc9f91e1a3ddcf86825f1cd5dc758d8cde2a74b
Firrow <firrowmb@gmail.com>
2025-02-12 19:43:53 +0100

    Add sprite trigger ending

--- Project_Mira_DaU/Source/Project_Mira_DaU/Private/Game/TriggerEnding.cpp
+++ Project_Mira_DaU/Source/Project_Mira_DaU/Private/Game/TriggerEnding.cpp
@@ -5,10 +5,6 @@ ATriggerEnding::ATriggerEnding()
 {
 	Tags.Add(TEXT("EndingTrigger"));
 
-	TriggerWithEnding = CreateDefaultSubobject<UBoxComponent>(TEXT("CapsuleRessource"));
-	TriggerWithEnding->SetBoxExtent(FVector(1.f, 1.f, 1.f));
-	TriggerWithEnding->SetupAttachment(RootComponent);
-
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
