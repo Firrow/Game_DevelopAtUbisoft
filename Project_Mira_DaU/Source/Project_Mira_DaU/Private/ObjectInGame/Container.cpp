047ee5aabc39996ed6abe1f4b805ce0f8873b206
Firrow <firrowmb@gmail.com>
2025-02-12 18:28:54 +0100

    Fix bug : some chest didn't opened

--- Project_Mira_DaU/Source/Project_Mira_DaU/Private/ObjectInGame/Container.cpp
+++ Project_Mira_DaU/Source/Project_Mira_DaU/Private/ObjectInGame/Container.cpp
@@ -5,10 +5,6 @@
 
 AContainer::AContainer()
 {
-    CapsuleColliderRessource = CreateDefaultSubobject<USphereComponent>(TEXT("CapsuleRessource"));
-    CapsuleColliderRessource->InitSphereRadius(5.f);
-    CapsuleColliderRessource->SetupAttachment(RootComponent);
-
     RessourcePointSpawn = CreateDefaultSubobject<USphereComponent>(TEXT("RessourcePointSpawn"));
     RessourcePointSpawn->InitSphereRadius(1.f);
     RessourcePointSpawn->SetupAttachment(CapsuleColliderRessource);
@@ -26,8 +22,6 @@ void AContainer::Effect()
         else
         {
             //TODO : Lancer l'animation du coffre en BP
-
-            GEngine->AddOnScreenDebugMessage(-1, 50000.f, FColor::Yellow, FString::Printf(TEXT("COFFRE OUVERT !")));
 
             FRotator Rotation(0.0f, 0.0f, 0.0f);
             GetWorld()->SpawnActor<ARessource>(RessourceInside.Last(), RessourcePointSpawn->GetComponentLocation(), Rotation);
