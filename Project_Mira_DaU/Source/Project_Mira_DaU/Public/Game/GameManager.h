f08ae34aff559f5312df750765eb0531069cebb9
Firrow <firrowmb@gmail.com>
2025-02-22 15:37:05 +0100

    Adding gear count in UI

--- Project_Mira_DaU/Source/Project_Mira_DaU/Public/Game/GameManager.h
+++ Project_Mira_DaU/Source/Project_Mira_DaU/Public/Game/GameManager.h
@@ -6,22 +6,26 @@
 #include "Blueprint/UserWidget.h"
 #include "GameManager.generated.h"
 
+// Delegate dynamique pour notifier les changements de PlayerGearsQuantity
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnGearsQuantityChanged, int, PlayerGearsQuantity);
+
+
 UCLASS()
 class PROJECT_MIRA_DAU_API AGameManager : public AActor
 {
 	GENERATED_BODY()
 	
 private:	
-
 	int RealTimeValue;
 	int GameTimeValue;
-	int PlayerGearsQuantity;
+	int PlayerGearsQuantity = 0;
 
 	int SECONDS_IN_A_DAY = 86400;
 	int TOTAL_PLAYING_TIME = 10; //TODO : Set to 3600 or 1800 when tests are finished
 
 	FTimerHandle timer;
 	int translateSecondsIRLTimeToGameTime = SECONDS_IN_A_DAY / TOTAL_PLAYING_TIME;
+	bool UIIsDisplay = false;
 
 	UPROPERTY()
 	UUserWidget* PlayerInformationsWidget;
@@ -35,6 +39,7 @@ private:
 	AGameManager();
 	void TimerManager();
 	int GetTimeInGame(const int& RealTime);
+	void FinishedGame();
 	
 
 protected:
@@ -64,6 +69,8 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Element in Map")
 	TMap<int, int> RessourcesQuantity;
 
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+	FOnGearsQuantityChanged OnGearsQuantityChanged;
 
 
 	int GetRealTimeValue() const;
